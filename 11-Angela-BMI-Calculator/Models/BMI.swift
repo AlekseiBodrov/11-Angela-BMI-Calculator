8254025237e58e17f4a4cc84fdaf9bd031239e88
Aleksei <aleksei@mail.ru>
2023-02-04 23:18:38 +0300

    Complete

---
+++ 11-Angela-BMI-Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  11-Angela-BMI-Calculator
+//
+//  Created by Алексей on 04.02.2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
